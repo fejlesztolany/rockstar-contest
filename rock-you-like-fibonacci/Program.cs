6b43440b3a025429099717f647c98a1f5dba2f5a
Farkas Péter <farkas.peter1995@gmail.com>
2019-01-30 22:59:59 +0100

    Created Rock You Like Fibonacci

---
+++ rock-you-like-fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static int Fibonacci(int num)
+        {
+            if (num <= 0) return 0;
+            if (num == 1) return 1;
+            return Fibonacci(num - 1) + Fibonacci(num - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj meg egy számot");
+            int szam = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fibonacci(szam));
+            Console.ReadKey();
+        }
+    }
+}
